6372255eb6a2a1571f0c5fea254d3e9024b43574
StupidLamer <grisga2001@gmail.com>
2020-06-21 01:31:49 +0800

    new tasks

---
+++ learn/lambda.py
@@ -0,0 +1,21 @@
+# def get_square(num):
+# 	return num ** 2
+
+# print(get_square(5))
+
+# get_square = lambda num: num ** 2
+# print(get_square(5))
+
+# print((lambda num: num ** 2)(7))
+
+l = [1, 2, 3]
+
+# def get_double(l):
+# 	return [i * 2 for i in l]
+
+# print(get_double(l))
+
+def get_double(l):
+	return list(map(lambda num: num * 2, l))
+
+print(get_double(l))
